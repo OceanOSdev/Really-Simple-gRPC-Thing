e94764b2e7e9fb4a92972a649ba00526a49459e6
Thomas Maloney <thomasjmaloney99@gmail.com>
2026-02-21 20:48:12 -0600

    Add calc client

---
+++ GrpcCalculatorClient/Program.cs
@@ -0,0 +1,30 @@
+﻿using Grpc.Net.Client;
+using GrpcCalculatorClient;
+
+using var channel = GrpcChannel.ForAddress("https://localhost:7169");
+var client = new Calculator.CalculatorClient(channel);
+
+Console.WriteLine("Enter first number:");
+var input = Console.ReadLine();
+int lhs = 0;
+
+if (!int.TryParse(input, out lhs))
+{
+  Console.WriteLine("Error parsing int. Exiting...");
+  return;
+}
+
+Console.WriteLine("Enter second number:");
+input = Console.ReadLine();
+int rhs = 0;
+
+if (!int.TryParse(input, out rhs))
+{
+  Console.WriteLine("Error parsing int. Exiting...");
+  return;
+}
+
+var reply = await client.AddAsync(
+    new AddRequest { Lhs = lhs, Rhs = rhs });
+
+Console.WriteLine($"{lhs} + {rhs} = " + reply.Sum);
